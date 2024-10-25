9dae9cb08160d756126fdb318ad36a8090c52d8e
7Limes <milesb2288@gmail.com>
2024-10-25 16:56:11 -0400

    Add level saving

--- Buttons/menubar.gd
+++ Buttons/menubar.gd
@@ -6,13 +6,22 @@ extends Node
 @onready var root = $".."
 
 
+func wall_data_to_dict(all_wall_data: Array) -> Dictionary:
+	var dict = {'static': [], 'dynamic': []}
+	for wall_data: WallData in all_wall_data:
+		if wall_data.type == WallData.WallType.STATIC:
+			dict['static'].append(wall_data.data)
+		else:  # Dynamic
+			dict['dynamic'].append(wall_data.data)
+	return dict
+
+
 func _on_load_button_pressed() -> void:
 	load_file_dialog.add_filter('*.json', 'Factory Game Level')
 	load_file_dialog.show()
 
 
 func _on_load_file_dialog_file_selected(path: String) -> void:
-	get_tree().paused = false
 	print(path)
 
 
@@ -22,8 +31,10 @@ func _on_save_button_pressed() -> void:
 
 
 func _on_save_file_dialog_file_selected(path: String) -> void:
-	get_tree().paused = false
-	print(path)
+	var wall_data_dict = wall_data_to_dict(root.all_wall_data)
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_string(JSON.stringify(wall_data_dict))
+	print('Save done!')
 
 
 func _on_new_wall_button_pressed() -> void:
